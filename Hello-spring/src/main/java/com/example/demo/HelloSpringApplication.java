6d3b79f7a5c52645d09ccb514082f2c08a66958d
cellist01 <cellist01@gmail.com>
2022-05-18 11:47:50 +0900

    push

--- Hello-spring/src/main/java/com/example/demo/HelloSpringApplication.java
+++ Hello-spring/src/main/java/com/example/demo/HelloSpringApplication.java
@@ -2,6 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class HelloSpringApplication {
@@ -11,4 +16,12 @@ public class HelloSpringApplication {
 		
 		System.out.println("Hello World");	}
 
+}
+@RestController
+class GreetingController {
+    
+    @RequestMapping("/hello/{name}")
+    String hello(@PathVariable String name) {
+        return "Hello, " + name + "!";
+    }
 }
