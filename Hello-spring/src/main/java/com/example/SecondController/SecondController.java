a21975a03cfcc08778373985bd9751d48d5e499b
cellist01 <cellist01@gmail.com>
2022-05-18 14:30:44 +0900

    test test

--- Hello-spring/src/main/java/com/example/SecondController/SecondController.java
+++ Hello-spring/src/main/java/com/example/SecondController/SecondController.java
@@ -1,5 +1,7 @@
 package com.example.SecondController;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
